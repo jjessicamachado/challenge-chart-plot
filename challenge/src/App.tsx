4222b3553fb335a9296088a475c8db52e31cb134
Jessica Machado <jessica.machado@radixeng.com.br>
2021-03-06 17:17:30 -0300

    initial configs

---
+++ challenge/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App" >
+      <header className="App-header">
+        Teste
+      </header>
+    </div>
+  );
+}
+
+export default App;
